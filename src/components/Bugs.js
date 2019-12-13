47c4de722674aa2e2d677d5110ce5a7c8e587b2d
Kristina Zbinden <53029467+kristinazbinden@users.noreply.github.com>
2019-12-13 14:57:50 -0800

    Added components and app.js

---
+++ src/components/Bugs.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Bugs extends React.Component {
+    render () {
+        return (
+            <aside>
+                <h1>I'm Bugs component</h1>
+            </aside>
+        )
+    }
+}
+
+export default Bugs
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Header extends React.Component {
+    render () {
+        return (
+            <aside>
+                <h1>I'm Header component</h1>
+            </aside>
+        )
+    }
+}
+
+export default Header
