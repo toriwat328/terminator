0bf37de28629ffe3338b8a2a579f6e3bf9626fa5
Kristina Zbinden <53029467+kristinazbinden@users.noreply.github.com>
2019-12-15 15:23:46 -0800

    Front end working except for Form

--- src/App.js
+++ src/App.js
@@ -8,9 +8,67 @@ class App extends React.Component {
         super(props)
         this.state = {
             view: {
-                page: 'bugs'
+                page: 'home',
+                pageTitle: 'Bug Log'
+            },
+            formInputs: {
+                id: null,
+                title: null,
+                description: null,
+                projectid: null,
+                datefound: null,
+                screenshot: null,
+                isresolved: null,
+                solution: null,
+
             }
         }
+    }
+
+    handleView = (view, bugData) => {
+        let pageTitle = ''
+        let formInputs = {
+            id: null,
+            title: '',
+            description: '',
+            projectid: null,
+            datefound: '',
+            screenshot: '',
+            isresolved: false,
+            solution: ''
+        }
+        switch(view) {
+            case 'home':
+                pageTitle = 'Bug Log'
+                break
+            case 'addIssue':
+                pageTitle = 'Record New Bug'
+                break
+            case 'editIssue':
+                pageTitle = 'Update Bug Record'
+                formInputs = {
+                    id: bugData.id,
+                    title: bugData.title,
+                    description: bugData.description,
+                    projectid: bugData.projectid,
+                    datefound: bugData.datefound,
+                    screenshot: bugData.screenshot,
+                    isresolved: bugData.isresolved,
+                    solution: bugData.solution,
+                }
+                break
+            default:
+                break
+            }
+
+            this.setState({
+                view: {
+                    page: view,
+                    pageTitle: pageTitle
+                },
+                formInputs: formInputs
+            })
+
     }
 
 
@@ -19,8 +77,12 @@ class App extends React.Component {
             <div className="outer-container">
                 <Header/>
                 <div className="main-container">
-                    <Aside/>
-                    <Main/>
+                    <Aside handleView={this.handleView}/>
+                    <Main
+                        view={this.state.view}
+                        handleView={this.handleView}
+                        formInputs={this.state.formInputs}
+                    />
                 </div>
             </div>
         )
