652c57a479711511ff167ac04327c0bc6f445e3c
Kristina Zbinden <53029467+kristinazbinden@users.noreply.github.com>
2019-12-16 08:58:57 -0800

    Added a bit of css styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import bugs from './bug.png'
 
 class Header extends React.Component {
     render () {
@@ -6,6 +7,8 @@ class Header extends React.Component {
             <header>
                 <span>squash coding bugs with the</span>
                 <h1>TERMINATOR</h1>
+                <img src={bugs} alt="bugs running across screen" />
+
             </header>
         )
     }
