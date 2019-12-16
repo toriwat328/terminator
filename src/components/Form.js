1d57eed507f37762764eb3cc1d32c2cabdd2eb27
Kristina Zbinden <53029467+kristinazbinden@users.noreply.github.com>
2019-12-15 16:53:53 -0800

    Got create/update form working

--- src/components/Form.js
+++ src/components/Form.js
@@ -13,17 +13,19 @@ class Form extends React.Component {
             screenshot: '',
             isresolved: false,
             solution: ''
-        }
-        this.handleChange = this.handleChange.bind(this)
+        };
+        this.handleChange = this.handleChange.bind(this);
     }
 
-    handleChange = (e) => {
-        this.setState({[e.target.id] : e.target.value})
+    handleChange(event){
+        this.setState({
+            [event.target.name]: event.target.value
+        });
     }
 
-    handleSubmit = (e) => {
-        e.preventDefault()
-        console.log('hello');
+    handleSubmit = (event) => {
+        event.preventDefault()
+        console.log(this.state);
         if(this.props.view.page === 'addIssue') {
             this.props.handleCreate(this.state)
         } else if(this.props.view.page === 'editIssue') {
@@ -50,32 +52,34 @@ class Form extends React.Component {
             <form onSubmit={this.handleSubmit}>
                 <label>
                     Title
-                    <input type="text" placeholder="Title" id="bugTitle"
+                    <input type="text" placeholder="Title" id="bugTitle" defaultValue={this.state.title} name="title"
                      onChange={this.handleChange}/>
                 </label>
                 <label>
                     Description
-                    <input type="text" ref={this.input} placeholder="Description" id="bugDescription"
+                    <input type="text" placeholder="Description" id="bugDescription" defaultValue={this.state.description} name="description"
                     onChange={this.handleChange}/>
                 </label>
                 <label>
                     Date Found
-                    <input type="text" placeholder="Date Found" id="bugDateFound"
+                    <input type="text" placeholder="Date Found" id="bugDateFound" defaultValue={this.state.datefound} name="datefound"
                     onChange={this.handleChange}/>
                 </label>
                 <label>
                     Screenshot
-                    <input type="text" placeholder="Screenshot" id="bugScreenshot"
+                    <input type="text" placeholder="Screenshot" id="bugScreenshot" defaultValue={this.state.screenshot} name="screenshot"
                     onChange={this.handleChange}/>
                 </label>
                 <label>
                     Resolved
-                    <input type="text" placeholder="Resolved" id="bugIsResolved"
+                    <input type="text" placeholder="Resolved" id="bugIsResolved" defaultValue={this.state.isresolved}
+                    name="isresolved"
                     onChange={this.handleChange}/>
                 </label>
                 <label>
                     Solution
-                    <input type="text" placeholder="Solution" id="bugSolution"
+                    <input type="text" placeholder="Solution" id="bugSolution" defaultValue={this.state.solution}
+                    name="solution"
                     onChange={this.handleChange}/>
                 </label>
                 <input type="submit" value="Log Bug"/>
