926c2b72a36b2456bb381137b48edd74cb905ae7
Kristina Zbinden <53029467+kristinazbinden@users.noreply.github.com>
2019-12-17 11:22:11 -0800

    button styling redone

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,6 @@
 import React from 'react'
-import Projects from './Projects.js'
+import Bugs from './Bugs.js'
 import Form from './Form.js'
-import ProjectDetails from './ProjectDetails.js'
-import BugDetails from './BugDetails.js'
 
 let baseUrl = '';
 if (process.env.NODE_ENV === 'development') {
@@ -15,19 +13,8 @@ class Main extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            projects: [],
-            bugs: [], //bug array
-            projectShow: [], //individual project
-            bugShow: [] //individual bug
+            bugs: []
         }
-    }
-
-    fetchProjects = () => {
-        fetch(`${baseUrl}/projects`)
-        .then(data=>data.json())
-        .then(jData=> {
-            this.setState({projects:jData})
-        }).catch(err=>console.log(err))
     }
 
     fetchBugs = () => {
@@ -38,35 +25,8 @@ class Main extends React.Component {
         }).catch(err=>console.log(err))
     }
 
-    handleCreate = (createProject) => {
+    handleCreate = (createBug) => {
         console.log('this is handleCreate');
-        console.log(createProject);
-        fetch(`${baseUrl}/projects`, {
-            body: JSON.stringify(createProject),
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json, text/plain, */*',
-                'Content-Type': 'application/json'
-            }
-        })
-        .then(createdProject => {
-            return createdProject.json();
-        })
-        .then(jsonedProject => {
-            console.log(jsonedProject);
-            this.setState(prevState => {
-                prevState.projects = jsonedProject
-                return { projects: prevState.projects}
-            }, () => {
-                console.log(this.state.projects);
-                this.props.handleView('home', '', '')
-            })
-        })
-        .catch(err => console.log(err))
-    }
-
-    handleCreateBug = (createBug) => {
-        console.log(createBug);
         fetch(`${baseUrl}/issues`, {
             body: JSON.stringify(createBug),
             method: 'POST',
@@ -79,38 +39,18 @@ class Main extends React.Component {
             console.log('bug created');
             return createdBug.json()
         })
-        .then(jsonedBugs => {
-            console.log(jsonedBugs);
+        .then(jsonedBug => {
+            this.props.handleView('home')
             this.setState(prevState => {
-                prevState.bugs = jsonedBugs
+                prevState.bugs = jsonedBug
                 return { bugs: prevState.bugs}
-            }, () => {
-                console.log(this.state.bugs);
-                this.props.handleView('home', '', '')
             })
         })
         .catch(err => console.log(err))
     }
 
-    handleUpdate = (projectUpdate) => {
-        fetch(`${baseUrl}/projects/${projectUpdate.id}`, {
-            body: JSON.stringify(projectUpdate),
-            method: 'PUT',
-            headers: {
-                'Accept': 'application/json, text/plain, */*',
-                'Content-Type': 'application/json'
-            }
-        })
-            .then(updatedProject => {
-                this.props.handleView('home', '', '')
-                this.fetchProjects()
-            })
-    }
-
-    handleUpdateBug = (bugUpdate, bugId) => {
-        console.log(bugUpdate)
-        console.log(bugId);
-        fetch(`${baseUrl}/issues/${bugId}`, {
+    handleUpdate = (bugUpdate) => {
+        fetch(`${baseUrl}/issues/${bugUpdate.id}`, {
             body: JSON.stringify(bugUpdate),
             method: 'PUT',
             headers: {
@@ -119,26 +59,9 @@ class Main extends React.Component {
             }
         })
             .then(updatedBug => {
-                this.props.handleView('home', '', '')
+                this.props.handleView('home')
                 this.fetchBugs()
             })
-    }
-
-    deleteProject = (id) => {
-        fetch(`${baseUrl}/projects/${id}`, {
-            method: 'DELETE',
-            headers: {
-                'Accept': 'application/json, text/plain, */*',
-                'Content-Type': 'application/json'
-            }
-        })
-            .then(json => {
-                this.setState(prevState => {
-                    const projects = prevState.projects.filter(project => project.id !== id)
-                    return { projects }
-                })
-            })
-            .catch(err => console.log(err))
     }
 
     deleteBug = (id) => {
@@ -153,44 +76,13 @@ class Main extends React.Component {
                 this.setState(prevState => {
                     const bugs = prevState.bugs.filter(bug => bug.id !== id)
                     return { bugs }
-                }, () => {
-                    this.props.handleView('home', '', '')
                 })
             })
             .catch(err => console.log(err))
     }
 
-    showProject = (project) => {
-        fetch(`${baseUrl}/projects/${project.id}`)
-        .then(data=> data.json())
-        .then(showjData=> {
-            this.setState({
-                projectShow: showjData
-            }, () => {
-                this.props.handleView('showProject', this.state.projectShow, '' );
-                console.log(this.state.projectShow);
-            })
-        }).catch(err=>console.log(err))
-
-    }
-
-    showBug = (bug) => {
-        fetch(`${baseUrl}/issues/${bug.id}`)
-        .then(data=> data.json())
-        .then(showBugjData=> {
-            this.setState({
-                bugShow: showBugjData
-            }, () => {
-                this.props.handleView('showBug', '', this.state.showBug);
-                console.log(this.state.bugShow);
-            })
-        }).catch(err=>console.log(err))
-
-    }
-
-
     componentDidMount() {
-        this.fetchProjects()
+        this.fetchBugs()
     }
 
 
@@ -199,34 +91,20 @@ class Main extends React.Component {
             <main>
                 <h2>{this.props.view.pageTitle}</h2>
 
-
-
-
                 { this.props.view.page === 'home'
-                    ? this.state.projects.map((projectData) => (
-                        <Projects
-                            key={projectData.id}
-                            projectData={projectData}
+                    ? this.state.bugs.map((bugData) => (
+                        <Bugs
+                            key={bugData.id}
+                            bugData={bugData}
                             handleView={this.props.handleView}
-                            deleteProject={this.deleteProject}
-                            showProject={this.showProject}
-
+                            deleteBug={this.deleteBug}
                         />
-                    )): this.props.view.page === 'showProject' ?
-                    <ProjectDetails  showData={this.state.projectShow} handleView={this.props.handleView} showBug={this.showBug} deleteBug={this.deleteBug}/>
-                    : this.props.view.page === 'showBug' ?
-                    <BugDetails showData={this.state.projectShow} showBugData={this.state.bugShow} handleView={this.props.handleView}/>
-
-
+                    ))
                     : <Form
                         handleCreate={this.handleCreate}
                         handleUpdate={this.handleUpdate}
-                        formInputsProjects={this.props.formInputsProjects}
-                        formInputsBugs={this.props.formInputsBugs}
+                        formInputs={this.props.formInputs}
                         view={this.props.view}
-                        handleCreateBug={this.handleCreateBug}
-                        handleUpdateBug={this.handleUpdateBug}
-                        showBugData={this.state.bugShow}
                     />
                 }
             </main>
