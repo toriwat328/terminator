e36a2d9bfdd45001a998fbc32d2f476d7269855d
Kristina Zbinden <53029467+kristinazbinden@users.noreply.github.com>
2019-12-17 08:55:56 -0800

    Commiting for next to final push to Heroku

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Footer extends React.Component {
+    render () {
+        return (
+            <footer>
+                Copyright 2019
+            </footer>
+        )
+    }
+}
+
+export default Footer
