34cb3f18d7ef42ec16d2ee7cc89946be917cdb4e
toriwat328 <toriwat328@gmail.com>
2019-12-18 12:46:02 -0500

    added heroku base url

--- src/components/Main.js
+++ src/components/Main.js
@@ -9,7 +9,7 @@ let baseUrl = '';
 if (process.env.NODE_ENV === 'development') {
     baseUrl = 'http://localhost:8888'
 } else {
-    console.log('this is for heroku');
+    baseUrl = 'https://bug-terminator-api.herokuapp.com/api';
 }
 
 class Main extends React.Component {
